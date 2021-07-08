ae0435f524572e473ce53840e44f06f1f8f8b175
ibtashamalee <aleejaan562@gmail.com>
2021-07-09 00:54:27 +0500

    deletes time entries model

--- models/TimeEntriesSchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-const Entries = mongoose.Schema({
-    clockIn: Date,
-    clockOut: Date,
-})
-
-module.exports = TimeEntries = mongoose.model('timeEntriesSchema', Entries);
